7db60decda9bb5ffd61364456c0846f8fde62413
shafiq1511999 <33915612+shafiq1511999@users.noreply.github.com>
2018-01-18 09:40:25 +0800

    Perfectly Works

--- Lab-Exercise-2.cpp
+++ Lab-Exercise-2.cpp
@@ -3,31 +3,36 @@
 
 #include <iostream>
 using namespace std;
-
-int main()
+int main ()
 {
 
-	int i=1,u=1,sum=0;
-	cout<<"The perfect numbers between 1 to 500 are: "<<endl;
+int i=1;
+int u;
+int sum;
+cout<<"The perfect numbers between 1 to 500 are: "<<endl;
+    for (i=1; i<=500; i++) 
+    {
+        u=1;
+        sum=0;
 
-	while(i<=500)
+        while (u<i)
+        {
+            if (i%u == 0)
+                sum = sum + u;
+            u++;
+        }
 
-		while(u<=500)
+        if (sum==i)
+        {
+            u= 1;
 
-			if(u<i)
-
-				if(i%u=)
-					sum=sum+u;
-
-	u++
-
-		if(sum=i)
-			
-			cout<<i<<" "<<endl;
-
-	i++
-		u=1;
-		sum=0;
-
-
+            while (u<i)
+            {
+                if (i%u == 0)
+                    cout << i << " = " << u << endl;
+                u++;
+            }
+        }
+    }
+return 0;
 }
