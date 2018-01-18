fa67a320ce22c3af23f833a09f9986d51387207f
shafiq1511999 <33915612+shafiq1511999@users.noreply.github.com>
2018-01-18 10:00:03 +0800

    Perfectly Works...XD

--- Lab-Exercise-4.cpp
+++ Lab-Exercise-4.cpp
@@ -1,24 +1,23 @@
 //Display the pattern using numebr repeating for a row
 //fix and complete the program below.
 
-
 #include <iostream>
-
-
+using namespace std;
 int main()
 {
+   int rows;
 	cout<<"Display the pattern"<<endl;
 	cout<<"-------------------"<<endl;
+    cout<< "Input No of Rows: ";
+    cin >> rows;
 
-	i,j,rows;
-	cout<<"Input number of rows :"<<endl;
-
-
-	for(i=1;i<=rows;i++)
-	{
-	for(j=1;j<=i;j++)
-		
-	cout<<endl;
-	}
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
 }
-	
